effd7ea73ab827aac982d36bdb5a858640ffd5ab
mariaeduardatc <81043486+mariaeduardatc@users.noreply.github.com>
2022-07-28 15:10:03 -0300

    final project code

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,21 +1,156 @@
-import { useRoute } from "@react-navigation/native";
-import { VStack, Text } from "native-base";
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
+import { VStack, Text, HStack, useTheme, ScrollView, Box } from "native-base";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import firestore from '@react-native-firebase/firestore';
+import { CircleWavyCheck, Hourglass, DesktopTower, Clipboard } from "phosphor-react-native";
+
+import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO'
 import { Header } from "../components/Header";
+import { OrderProps } from "../components/Order";
+import { dateFormat } from "../utils/firestoreDateFormat";
+import { Loading } from "../components/Loading";
+import { CardDetails } from '../components/CardDetails'
+import { Input } from "../components/Input";
+import { Button}  from '../components/Button'
 
 type RouteParams = {
     orderId: string;
 }
 
+type OrderDetails = OrderProps & {
+    description: string;
+    solution: string;
+    closed: string;
+}
+
 export function Details(){
+    const [solution, setSolution] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
+
+    const navigation = useNavigation();
+    const {colors} = useTheme();
     const route = useRoute();
+    
     const {orderId} = route.params as RouteParams;
+
+    function handleOrderClose(){
+        if(!solution){
+            return Alert.alert('Solicitação', 'Informe a solução para encerrar a solicitação'); 
+        }
+
+        firestore()
+        .collection<OrderFirestoreDTO>('orders')
+        .doc(orderId)
+        .update({
+            status: 'closed',
+            solution,
+            closed_atw: firestore.FieldValue.serverTimestamp()
+        })
+        .then(() => {
+            Alert.alert('Solicitação', 'Solicitação encerrada')
+            navigation.goBack();
+        })
+        .catch((error) => {
+            console.log(error)
+            Alert.alert('Solicitação', 'Não foi possível encerrar a solicitação');
+        });
+
+    }
+
+    useEffect(() => {
+        firestore()
+        .collection<OrderFirestoreDTO>('orders')
+        .doc(orderId)
+        .get()
+        .then((doc) => {
+            const {patrimony, description, status, created_at, closed_atw} = doc.data();
+
+            const closed = closed_atw ? dateFormat(closed_atw) : null;
+
+            setOrder({
+                id: doc.id,
+                patrimony,
+                description,
+                status,
+                solution,
+                when: dateFormat(created_at),
+                closed
+            });
+
+            setIsLoading(false);
+        });
+    }, []);
+
+    if (isLoading) {
+        return <Loading/>
+    }
+
     return(
         <VStack flex={1} bg="gray.700">
-            <Header title='solicitação'/>
-            <Text>
-                {orderId}
-            </Text>
+            <Box px={6} bg='gray.600'>
+                <Header title='solicitação'/>
+            </Box>
+            <HStack bg='gray.500' justifyContent='center' p={4}>
+                {
+                    order.status === 'closed'
+                    ? <CircleWavyCheck size={22} color={colors.green[300]} />
+                    : <Hourglass size={22} color={colors.secondary[700]} />
+                }
+                <Text
+                    fontSize = 'sm'
+                    color={order.status === 'closed' ? colors.green[300] : colors.secondary[700]}
+                    ml={2}
+                    textTransform='uppercase'
+                >
+                    {order.status === 'closed' ? 'Finalizado' : "Em andamento" }
+                </Text>
+            </HStack>
 
+            <ScrollView mx={5} showsHorizontalScrollIndicator={false}>
+                <CardDetails
+                    title='equipamento'
+                    description = {`Patrimônio ${order.patrimony}`}
+                    icon = {DesktopTower}
+                    footer={order.when}
+                />
+
+                <CardDetails
+                    title='descrição do problema'
+                    description = {order.description}
+                    icon = {Clipboard}
+                />  
+
+                <CardDetails
+                    title='solução'
+                    icon = {CircleWavyCheck}
+                    description={order.solution}
+                    footer={order.closed && `Encerrado em ${order.closed}`}
+                >
+                    {
+                        order.status === 'open' &&
+                        <Input 
+                            placeholder="Descrição da solução"
+                            onChangeText = {setSolution}
+                            h={24}
+                            textAlignVertical='top'
+                            multiline
+                        />
+                    }
+                </CardDetails>  
+
+            </ScrollView>
+            
+
+            {
+                order.status === 'open' && 
+                <Button
+                    title="Encerrar solicitação"
+                    m={5}
+                    onPress={handleOrderClose}
+                />
+            }
         </VStack>
     );
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
-import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from "native-base";
-import { SignOut } from "phosphor-react-native";
-import { ChatTeardropText } from "phosphor-react-native";
+import { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import { useNavigation } from '@react-navigation/native';
+import { HStack, IconButton, VStack, useTheme, Text, Heading, FlatList, Center } from 'native-base';
+import { SignOut } from 'phosphor-react-native';
+import { ChatTeardropText } from 'phosphor-react-native';
 
-import Logo from '../assets/logo_secondary.svg'
+import { dateFormat } from '../utils/firestoreDateFormat';
 
-import {Filter} from '../components/Filter'
-import {Button} from '../components/Button'
-import {Order, OrderProps} from '../components/Order'
+import Logo from '../assets/logo_secondary.svg';
 
+import { Filter } from '../components/Filter';
+import { Button } from '../components/Button';
+import { Loading } from '../components/Loading';
+import { Order, OrderProps } from '../components/Order';
 
 
 
 export function Home() {
 
+    const [isLoading, setIsLoading] = useState(true)
     const [statusSelected, setStatusSelected] = useState<'open'|'closed'>('open');
     const [orders, setOrders] = useState <OrderProps[]>([]);
     
@@ -28,6 +34,42 @@ export function Home() {
     function handleOpenDetails(orderId: string){
         navigation.navigate('details', {orderId})
     }
+
+    function handleLogout(){
+        auth()
+        .signOut()
+        .catch(error => {
+            return Alert.alert('Sair', 'Não foi possível sair.');
+        });
+    }
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        const subscriber = firestore()
+        .collection('orders')
+        .where('status','==', statusSelected)
+        .onSnapshot(snapshot => {
+            const data = snapshot.docs.map(doc => {
+                const {patrimony, description, status, created_at} = doc.data();
+
+                return {
+                    id: doc.id,
+                    patrimony,
+                    description,
+                    status,
+                    when: dateFormat(created_at)
+
+                }
+            })
+
+            setOrders(data);
+            setIsLoading(false);
+        });
+
+        return subscriber;
+
+    }, [statusSelected]);
 
     return(
 
@@ -45,6 +87,7 @@ export function Home() {
 
                 <IconButton
                     icon={<SignOut size={26} color={colors.gray[300]}/>}
+                    onPress={handleLogout}
                 />
 
             </HStack>
@@ -75,22 +118,25 @@ export function Home() {
                     />
                 </HStack>
 
-                <FlatList
-                    data={orders}
-                    keyExtractor={item => item.id}
-                    renderItem={({item}) => <Order data={item} onPress={() => handleOpenDetails(item.id)}/>}
-                    contentContainerStyle={{paddingBottom: 100}}
-                    ListEmptyComponent={() => (
-                        <Center>
-                            <ChatTeardropText color={colors.gray[700]} size={40}/>
-                            <Text color='gray,300' fontSize='xl' mt={6} textAlign='center'>
-                               Você ainda não possui {'\n'}
-                               solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
-                                
-                            </Text> 
-                        </Center>
-                    )}
-                />
+                {
+                    isLoading ? <Loading />:
+                    <FlatList
+                        data={orders}
+                        keyExtractor={item => item.id}
+                        renderItem={({item}) => <Order data={item} onPress={() => handleOpenDetails(item.id)}/>}
+                        contentContainerStyle={{paddingBottom: 100}}
+                        ListEmptyComponent={() => (
+                            <Center>
+                                <ChatTeardropText color={colors.gray[700]} size={40}/>
+                                <Text color='gray,300' fontSize='xl' mt={6} textAlign='center'>
+                                Você ainda não possui {'\n'}
+                                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+                                    
+                                </Text> 
+                            </Center>
+                        )}
+                    />
+                }
                 <Button title='Nova solicitação' onPress={handleNewOrder}/>
             </VStack>
         
